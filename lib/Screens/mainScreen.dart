53573cad857f3b94640741dde73cac87e7933897
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-01-12 16:01:55 +0530

    initial commit

--- lib/Screens/mainScreen.dart
+++ lib/Screens/mainScreen.dart
@@ -1,9 +1,10 @@
 // ignore_for_file: file_names
 
+import 'package:clock_app/widgets/analog_clock.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({ Key? key }) : super(key: key);
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   _MainScreenState createState() => _MainScreenState();
@@ -12,6 +13,9 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: Analog_Clock()),
+    );
   }
 }
--- lib/widgets/analog_clock.dart
+++ lib/widgets/analog_clock.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: camel_case_types
 
-import 'package:flutter_analog_clock/flutter_analog_clock.dart';
 import 'package:flutter/material.dart';
+import 'package:analog_clock/analog_clock.dart';
 
 class Analog_Clock extends StatefulWidget {
-  const Analog_Clock({ Key? key }) : super(key: key);
+  const Analog_Clock({Key? key}) : super(key: key);
 
   @override
   _Analog_ClockState createState() => _Analog_ClockState();
@@ -13,29 +13,18 @@ class Analog_Clock extends StatefulWidget {
 class _Analog_ClockState extends State<Analog_Clock> {
   @override
   Widget build(BuildContext context) {
-    return FlutterAnalogClock(
-      dateTime: DateTime.now(),
-      dialPlateColor: Colors.white,
-      hourHandColor: Colors.black,
-      minuteHandColor: Colors.black,
-      secondHandColor: Colors.black,
-      numberColor: Colors.black,
-      borderColor: Colors.black,
-      tickColor: Colors.black,
-      centerPointColor: Colors.black,
-      showBorder: true,
-      showTicks: true,
-      showMinuteHand: true,
-      showSecondHand: true,
-      showNumber: true,
-      borderWidth: 8.0,
-      hourNumberScale: .10,
-      hourNumbers: ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'],
+    return AnalogClock(
+      decoration: BoxDecoration(color: Colors.black, shape: BoxShape.circle),
+      width: 292.0,
       isLive: true,
-      width: 200.0,
-      height: 200.0,
-      decoration: const BoxDecoration(),
-      child: Text('Clock'),
+      hourHandColor: Color(0xFFDC8B8B),
+      minuteHandColor: Color(0xFFDC8B8B),
+      showSecondHand: true,
+      showNumbers: false,
+      showTicks: true,
+      digitalClockColor: Colors.white,
+      showDigitalClock: true,
+      datetime: DateTime.now(),
     );
   }
 }
